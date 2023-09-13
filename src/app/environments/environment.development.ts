895fa45b785fa1a2cbf2ccdcb07c23d03489d9ce
Minh Trường <letruong147123@gmail.com>
2023-09-13 10:38:36 +0700

    base core

---
+++ src/app/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  env: 'local',
+  apiUrl: 'http://localhost:3000/api/v1',
+};
+import 'zone.js/plugins/zone-error'; // Included with Angular CLI.
