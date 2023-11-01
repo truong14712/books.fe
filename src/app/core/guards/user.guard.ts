36de2d1168b77eae6fbbe95f7f5fe90fc215b820
Minh Trường <letruong147123@gmail.com>
2023-11-01 10:05:01 +0700

    feat decentralization

---
+++ src/app/core/guards/user.guard.ts
@@ -0,0 +1,27 @@
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from '@core/services/auth/auth.service';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const isAuthenticated = this.authService.isAuthenticated();
+    if (isAuthenticated?.role === 'member' || isAuthenticated?.role === 'admin') {
+      return true;
+    } else {
+      this.router.navigate(['/']);
+      return false;
+    }
+  }
+}
