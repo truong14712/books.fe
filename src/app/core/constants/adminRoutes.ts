cbf71a4c647cd00fbc5506935e7519e399df4340
Minh Trường <letruong147123@gmail.com>
2023-11-08 10:13:37 +0700

    feat add admin routes

--- src/app/core/constants/adminRoutes.ts
+++ src/app/core/constants/adminRoutes.ts
@@ -1,16 +1,22 @@
 import { AddBookComponent } from '@pages/admin/book/add-book/add-book.component';
 import { AddCategoryComponent } from '@pages/admin/category/add-category/add-category.component';
 import { AddCouponComponent } from '@pages/admin/coupon/add-coupon/add-coupon.component';
+import { AddEventComponent } from '@pages/admin/event/add-event/add-event.component';
 import { DashboardComponent } from '@pages/admin/dashboard/dashboard.component';
+import { DetailOrderComponent } from '@pages/admin/order/detail-order/detail-order.component';
+import { DetailReportComponent } from '@pages/admin/report/detail-report/detail-report.component';
 import { ManagementBookComponent } from '@pages/admin/book/management-book/management-book.component';
 import { ManagementCategoryComponent } from '@pages/admin/category/management-category/management-category.component';
 import { ManagementCouponComponent } from '@pages/admin/coupon/management-coupon/management-coupon.component';
+import { ManagementEventComponent } from '@pages/admin/event/management-event/management-event.component';
+import { ManagementOrderComponent } from '@pages/admin/order/management-order/management-order.component';
+import { ManagementReportComponent } from '@pages/admin/report/management-report/management-report.component';
+import { ManagementUserComponent } from '@pages/admin/user/management-user/management-user.component';
 import { Routes } from '@angular/router';
 import { UpdateBookComponent } from '@pages/admin/book/update-book/update-book.component';
 import { UpdateCategoryComponent } from '@pages/admin/category/update-category/update-category.component';
 import { UpdateCouponComponent } from '@pages/admin/coupon/update-coupon/update-coupon.component';
-import { ManagementOrderComponent } from '@pages/admin/order/management-order/management-order.component';
-import { UpdateOrderComponent } from '@pages/admin/order/update-order/update-order.component';
+import { UpdateEventComponent } from '@pages/admin/event/update-event/update-event.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
@@ -59,8 +65,32 @@ const routes: Routes = [
     component: ManagementOrderComponent,
   },
   {
-    path: 'order/update/:id',
-    component: UpdateOrderComponent,
+    path: 'order/detail/:id',
+    component: DetailOrderComponent,
+  },
+  {
+    path: 'user',
+    component: ManagementUserComponent,
+  },
+  {
+    path: 'event',
+    component: ManagementEventComponent,
+  },
+  {
+    path: 'event/add',
+    component: AddEventComponent,
+  },
+  {
+    path: 'event/update/:id',
+    component: UpdateEventComponent,
+  },
+  {
+    path: 'report',
+    component: ManagementReportComponent,
+  },
+  {
+    path: 'report/detail/:id',
+    component: DetailReportComponent,
   },
 ];
 export default routes;
--- src/app/core/modules/admin/admin.module.ts
+++ src/app/core/modules/admin/admin.module.ts
@@ -1,13 +1,20 @@
 import { AddBookComponent } from '@pages/admin/book/add-book/add-book.component';
 import { AddCategoryComponent } from '@pages/admin/category/add-category/add-category.component';
 import { AddCouponComponent } from '@pages/admin/coupon/add-coupon/add-coupon.component';
+import { AddEventComponent } from '@pages/admin/event/add-event/add-event.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from '@pages/admin/dashboard/dashboard.component';
+import { DetailOrderComponent } from '@pages/admin/order/detail-order/detail-order.component';
+import { DetailReportComponent } from '@pages/admin/report/detail-report/detail-report.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ManagementBookComponent } from '@pages/admin/book/management-book/management-book.component';
 import { ManagementCategoryComponent } from '@pages/admin/category/management-category/management-category.component';
 import { ManagementCouponComponent } from '@pages/admin/coupon/management-coupon/management-coupon.component';
+import { ManagementEventComponent } from '@pages/admin/event/management-event/management-event.component';
 import { ManagementOrderComponent } from '@pages/admin/order/management-order/management-order.component';
+import { ManagementReportComponent } from '@pages/admin/report/management-report/management-report.component';
+import { ManagementUserComponent } from '@pages/admin/user/management-user/management-user.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -18,14 +25,16 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
-import { ModelUpdateBookHightLightComponent } from '@core/components/admin/model-update-book-hight-light/model-update-book-hight-light.component';
+import { NgChartsModule } from 'ng2-charts';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared-module/shared-module.module';
 import { UpdateBookComponent } from '@pages/admin/book/update-book/update-book.component';
 import { UpdateCategoryComponent } from '@pages/admin/category/update-category/update-category.component';
 import { UpdateCouponComponent } from '@pages/admin/coupon/update-coupon/update-coupon.component';
+import { UpdateEventComponent } from '@pages/admin/event/update-event/update-event.component';
 import { UpdateOrderComponent } from '@pages/admin/order/update-order/update-order.component';
 
 @NgModule({
@@ -36,12 +45,19 @@ import { UpdateOrderComponent } from '@pages/admin/order/update-order/update-ord
     UpdateCategoryComponent,
     AddBookComponent,
     UpdateBookComponent,
-    ModelUpdateBookHightLightComponent,
     AddCouponComponent,
     UpdateCouponComponent,
     ManagementCouponComponent,
     ManagementOrderComponent,
     UpdateOrderComponent,
+    DetailOrderComponent,
+    ManagementUserComponent,
+    ManagementEventComponent,
+    AddEventComponent,
+    UpdateEventComponent,
+    ManagementReportComponent,
+    DetailReportComponent,
+    DashboardComponent,
   ],
   imports: [
     FormsModule,
@@ -61,6 +77,8 @@ import { UpdateOrderComponent } from '@pages/admin/order/update-order/update-ord
     MatNativeDateModule,
     MatIconModule,
     MatDatepickerModule,
+    MatSortModule,
+    NgChartsModule,
   ],
 })
 export class AdminModule {}
