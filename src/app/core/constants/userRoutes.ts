d96d2eb5cb1e5e8f4beca22d2213399eb6e78065
Minh Trường <letruong147123@gmail.com>
2023-11-08 10:14:07 +0700

    feat add user routes

--- src/app/core/constants/userRoutes.ts
+++ src/app/core/constants/userRoutes.ts
@@ -1,12 +1,16 @@
 import { AddAddressComponent } from '@pages/user/add-address/add-address.component';
 import { ChangeInforMationComponent } from '@pages/user/change-infor-mation/change-infor-mation.component';
 import { ChangePasswordComponent } from '@pages/user/change-password/change-password.component';
-import { MyAddressComponent } from '@pages/user/my-address/my-address.component';
-import { Routes } from '@angular/router';
 import { CheckOutComponent } from '@pages/user/check-out/check-out.component';
+import { MyAddressComponent } from '@pages/user/my-address/my-address.component';
+import { OrderCancellationDetailComponent } from '@pages/user/order-cancellation-detail/order-cancellation-detail.component';
+import { OrderDetailComponent } from '@pages/user/order-detail/order-detail.component';
 import { PurchaseComponent } from '@pages/user/purchase/purchase.component';
+import { Routes } from '@angular/router';
+import { UpdateAddressComponent } from '@pages/user/update-address/update-address.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'profile/changeInformation', pathMatch: 'full' },
   {
     path: 'profile/changeInformation',
     component: ChangeInforMationComponent,
@@ -20,6 +24,10 @@ const routes: Routes = [
     component: AddAddressComponent,
   },
   {
+    path: 'profile/updateAddress/:id',
+    component: UpdateAddressComponent,
+  },
+  {
     path: 'profile/myAddress',
     component: MyAddressComponent,
   },
@@ -30,6 +38,14 @@ const routes: Routes = [
   {
     path: 'purchase',
     component: PurchaseComponent,
+  },
+  {
+    path: 'purchase/cancellation/:id',
+    component: OrderCancellationDetailComponent,
+  },
+  {
+    path: 'purchase/order/:id',
+    component: OrderDetailComponent,
   },
 ];
 export default routes;
--- src/app/core/modules/auth/auth.module.ts
+++ src/app/core/modules/auth/auth.module.ts
@@ -11,17 +11,25 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { ModelAddressComponent } from '@core/components/user/model-address/model-address.component';
+import { ModelCancelOrderComponent } from '@core/components/user/model-cancel-order/model-cancel-order.component';
+import { ModelReportComponent } from '@core/components/user/model-report/model-report.component';
+import { ModelReviewOrderComponent } from '@core/components/user/model-review-order/model-review-order.component';
+import { ModelShowVoucherComponent } from '@core/components/user/model-show-voucher/model-show-voucher.component';
+import { ModelShowVoucherDetailComponent } from '@core/components/user/model-show-voucher-detail/model-show-voucher-detail.component';
 import { MyAddressComponent } from '@pages/user/my-address/my-address.component';
 import { NgModule } from '@angular/core';
+import { OrderCancellationDetailComponent } from '@pages/user/order-cancellation-detail/order-cancellation-detail.component';
+import { OrderDetailComponent } from '@pages/user/order-detail/order-detail.component';
 import { PurchaseComponent } from '@pages/user/purchase/purchase.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared-module/shared-module.module';
-import { ModelShowVoucherComponent } from '@core/components/user/model-show-voucher/model-show-voucher.component';
-
+import { UpdateAddressComponent } from '@pages/user/update-address/update-address.component';
+import { UserSidebarProfileComponent } from '@core/components/user/user-sidebar-profile/user-sidebar-profile.component';
 @NgModule({
   declarations: [
     ChangeInforMationComponent,
@@ -31,6 +39,15 @@ import { ModelShowVoucherComponent } from '@core/components/user/model-show-vouc
     CheckOutComponent,
     ModelAddressComponent,
     PurchaseComponent,
+    ModelCancelOrderComponent,
+    ModelShowVoucherComponent,
+    ModelShowVoucherDetailComponent,
+    ModelReviewOrderComponent,
+    OrderCancellationDetailComponent,
+    OrderDetailComponent,
+    ModelReportComponent,
+    UserSidebarProfileComponent,
+    UpdateAddressComponent,
   ],
   imports: [
     BrowserAnimationsModule,
@@ -48,6 +65,7 @@ import { ModelShowVoucherComponent } from '@core/components/user/model-show-vouc
     MatListModule,
     SharedModule,
     RouterModule,
+    MatRadioModule,
   ],
 })
 export class AuthModule {}
